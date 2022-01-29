d9800937884342b3efc40700bf3e18ceb353f052
Michelle Van <michellevan@Michelles-Air.lan>
2022-01-29 13:47:36 -0500

    flask app with api call

---
+++ Makefile
@@ -0,0 +1,18 @@
+install:
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+
+test:
+	#python -m pytest -vv test_application.py
+
+lint:
+	pylint --disable=R,C application.py
+
+deploy:
+	echo "Deploying app"
+	eb deploy hello-env
+
+all: install lint test
+
+run: 
+	python main.py
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import requests
+import urllib.request, json
+
+application = Flask(__name__)
+    
+@application.route("/", methods=['POST', 'GET'])
+def index():
+    """Return a friendly HTTP greeting."""
+    return render_template('index.html')
+
+@application.route('/random', methods=['GET'])
+def random():
+    data = requests.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+
+    url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+
+    recipe = None
+    for movie in dict["drinks"]:
+
+        recipe = {
+            "drink": movie["strDrink"],
+            "instructions": movie["strInstructions"]
+        }    
+    return recipe
+    
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run(host='0.0.0.0', port=8000)
